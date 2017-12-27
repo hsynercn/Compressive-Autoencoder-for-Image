ef3dc368955c072e9c8daebd52c6cde2291d94f2
hsynercn <f0116050@std.yildiz.edu.tr>
2017-12-28 02:46:00 +0300

    Network export and import, image export and import added to utility.py, TestOutput folder added for trained networks and compressed data.

---
+++ Src/Application/utility.py
@@ -0,0 +1,132 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+def construct_network(layer_neurons):
+
+    weights = list()
+    biases = list()
+    for i in range(len(layer_neurons) - 1):
+        weights.append(tf.Variable(tf.random_normal([layer_neurons[i], layer_neurons[i + 1]])))
+
+    for i in range(len(layer_neurons)):
+        biases.append(tf.Variable(tf.random_normal([layer_neurons[i]])))
+
+    layers = list()
+    layers.append(tf.placeholder("float", [None, layer_neurons[0]]))
+
+    for i in range(1, len(layer_neurons)):
+        layers.append(tf.nn.sigmoid(tf.add(tf.matmul(layers[i - 1], weights[i -1 ]), biases[i])))
+
+    return layers[0], layers[(len(layers) - 1)//2], layers[len(layers) - 1]
+
+
+def train_network_mnist(autoencoder_architecture, model_file_path):
+    from tensorflow.examples.tutorials.mnist import input_data
+    mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+    learning_rate = 0.01
+    num_steps_old = 50000
+    num_steps = 400
+    batch_size = 256
+    test_steps = 10 * 4
+    display_step = 1000
+
+    input, encoder, decoder = construct_network(autoencoder_architecture)
+
+    target = input
+    loss = tf.reduce_mean(tf.pow(target - decoder, 2))
+    optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(loss)
+
+    init = tf.global_variables_initializer()
+
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        sess.run(init)
+        #for i in range(1, num_steps):
+        #    sample_cnt = 0
+        #    while sample_cnt < mnist.train.num_examples:
+        #        batch_x, _ = mnist.train.next_batch(batch_size)
+        #        _, loss_value = sess.run([optimizer, loss], feed_dict={input: batch_x})
+        #        sample_cnt += batch_size
+        #    print('Training Sample %i: Minibatch Loss: %f' % (i, loss_value))
+
+        for i in range(1, num_steps_old + 1):
+            batch_x, _ = mnist.train.next_batch(batch_size)
+            _, l = sess.run([optimizer, loss], feed_dict={input: batch_x})
+            if i % display_step == 0 or i == 1:
+                print('Step %i: Minibatch Loss: %f' % (i, l))
+
+        saver.save(sess, model_file_path)
+
+        test_cnt = 0
+        total_loss_value = 0
+        while test_cnt < mnist.test.num_examples:
+            batch_x, _ = mnist.train.next_batch(batch_size)
+            _, loss_value = sess.run([optimizer, loss], feed_dict={input: batch_x})
+            test_cnt += batch_size
+            total_loss_value = loss_value + total_loss_value
+
+        sess.close()
+        total_loss_value /= test_steps
+        print('Test Loss: %f' % (total_loss_value))
+
+def export_compressed_data(image_data, model_path, autoencoder_architecture, out_path):
+
+    input, encoder, decoder = construct_network(autoencoder_architecture)
+    with tf.Session() as sess:
+        saver = tf.train.Saver()
+        saver.restore(sess, model_path)
+        encoded_image_vector = sess.run(encoder, feed_dict={input: image_data})
+        np.savetxt(out_path, encoded_image_vector)
+        sess.close()
+
+def import_compressed_data(model_path, autoencoder_architecture,  import_path):
+
+
+    input, encoder, decoder = construct_network(autoencoder_architecture)
+    with tf.Session() as sess:
+        saver = tf.train.Saver()
+        saver.restore(sess, model_path)
+
+        recover_encoded_image_vector = np.loadtxt(import_path)
+        size = recover_encoded_image_vector.size
+        recover_encoded_image_vector = recover_encoded_image_vector.reshape(1, size)
+        decoded_image_vector = sess.run(decoder, feed_dict={encoder: recover_encoded_image_vector})
+        return decoded_image_vector
+
+
+
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+autoencoder_architecture = [784, 128, 64, 128, 784]
+
+model_path = "C:/Users/saruman/PycharmProjects/AutoEncoder/TestOutput/model_util_test_BASIC.ckpt"
+out_path = "C:/Users/saruman/PycharmProjects/AutoEncoder/TestOutput/compressed_data"
+batch_x, _ = mnist.train.next_batch(1)
+
+
+#train_network_mnist(autoencoder_architecture , model_path)
+
+#export_compressed_data( batch_x , model_path, autoencoder_architecture , out_path)
+
+canvas_orig = np.empty((28, 28))
+canvas_orig = batch_x.reshape([28, 28])
+print("Original Images")
+plt.figure(figsize=(1, 1))
+plt.imshow(canvas_orig, origin="upper", cmap="gray")
+plt.show()
+
+redata = import_compressed_data(model_path, autoencoder_architecture,  out_path)
+canvas_recon = np.empty((28, 28))
+canvas_recon = redata.reshape([28, 28])
+plt.figure(figsize=(1, 1))
+plt.imshow(canvas_recon, origin="upper", cmap="gray")
+plt.show()
+
+
+
+
---
+++ User_Interface.py
@@ -0,0 +1,12 @@
+
+from tkinter import *
+
+master = Tk()
+
+def callback():
+    print("click!")
+
+b = Button(master, text="OK", command=callback)
+b.pack()
+
+mainloop()
